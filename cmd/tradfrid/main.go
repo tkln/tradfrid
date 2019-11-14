8369f36e3f00246bac4e581319b89123b985ab40
Aapo Vienamo <aapo.vienamo@iki.fi>
2019-11-14 22:34:45 +0200

    tradfrid: Register ZigBee devices

--- cmd/tradfrid/main.go
+++ cmd/tradfrid/main.go
@@ -5,14 +5,35 @@ import (
     zmq "github.com/pebbe/zmq4"
     "github.com/golang/protobuf/proto"
     "tradfrid/proto"
+    "github.com/dyrkin/zigbee-steward"
+    "github.com/dyrkin/zigbee-steward/configuration"
 )
 
 func main() {
     log.Print("Starting")
+
+    znpConf := configuration.Default()
+    znpConf.Serial.PortName = "/dev/ttyACM0"
+    znpConf.PermitJoin = true
+
+    stewie := steward.New(znpConf)
+
+    handleZnpEvent := func() {
+        for {
+            select {
+            case device := <-stewie.Channels().OnDeviceRegistered():
+                log.Print("Register device: ", device)
+            }
+        }
+    }
+
+    go handleZnpEvent()
+
     server, _ := zmq.NewSocket(zmq.REP)
     defer server.Close()
     server.Bind("tcp://*:5432")
 
+    stewie.Start()
     for {
         req := &arith.Req{}
         req_data, _ := server.RecvBytes(0)
